eb5f031b83a1189dde9fe1a65db70355598ce2b4
Milija <jovanovichmiki@gmail.com>
2026-05-09 13:14:10 +0200

    Zadatak 6: Snimanje i organizacija fajlova na serveru

--- Server/MotorSessionWriter.cs
+++ Server/MotorSessionWriter.cs
@@ -9,6 +9,11 @@ namespace Server
     {
         private FileStream fileStream;
         private StreamWriter writer;
+        
+        // Dodato za rejects.csv
+        private FileStream rejectsFileStream;
+        private StreamWriter rejectsWriter;
+        
         private bool disposed;
 
         public string SessionDirectory { get; private set; }
@@ -27,6 +32,18 @@ namespace Server
             writer = new StreamWriter(fileStream);
             writer.AutoFlush = true;
             writer.WriteLine("Timestamp,Iq,Id,Coolant,ProfileId,Ambient,Torque");
+
+            // Kreiranje rejects.csv toka
+            rejectsFileStream = new FileStream(
+                Path.Combine(SessionDirectory, "rejects.csv"),
+                FileMode.Create,
+                FileAccess.Write,
+                FileShare.Read);
+
+            rejectsWriter = new StreamWriter(rejectsFileStream);
+            rejectsWriter.AutoFlush = true;
+            // Dodata kolona za razlog odbijanja
+            rejectsWriter.WriteLine("Timestamp,Iq,Id,Coolant,ProfileId,Ambient,Torque,RejectReason"); 
         }
 
         public void WriteSample(MotorSample sample)
@@ -49,6 +66,25 @@ namespace Server
                 sample.ProfileId.ToString(CultureInfo.InvariantCulture),
                 sample.Ambient.ToString(CultureInfo.InvariantCulture),
                 sample.Torque.ToString(CultureInfo.InvariantCulture)));
+        }
+
+        // Nova metoda za upis u rejects.csv
+        public void WriteReject(MotorSample sample, string reason)
+        {
+            if (disposed)
+            {
+                throw new ObjectDisposedException("MotorSessionWriter");
+            }
+
+            rejectsWriter.WriteLine(string.Join(",",
+                sample.Timestamp.ToString("O", CultureInfo.InvariantCulture),
+                sample.Iq.ToString(CultureInfo.InvariantCulture),
+                sample.Id.ToString(CultureInfo.InvariantCulture),
+                sample.Coolant.ToString(CultureInfo.InvariantCulture),
+                sample.ProfileId.ToString(CultureInfo.InvariantCulture),
+                sample.Ambient.ToString(CultureInfo.InvariantCulture),
+                sample.Torque.ToString(CultureInfo.InvariantCulture),
+                reason));
         }
 
         public void Dispose()
@@ -76,6 +112,19 @@ namespace Server
                 {
                     fileStream.Dispose();
                     fileStream = null;
+                }
+
+                // Zatvaranje rejects tokova
+                if (rejectsWriter != null)
+                {
+                    rejectsWriter.Dispose();
+                    rejectsWriter = null;
+                }
+
+                if (rejectsFileStream != null)
+                {
+                    rejectsFileStream.Dispose();
+                    rejectsFileStream = null;
                 }
             }
 
